a0e7805b2670923b58f5fc7a08594084ea789928
Rupert Redington <rupert@neontribe.co.uk>
2012-02-20 21:39:37 +0000

    Lint!

--- public/widgets/r4d.js
+++ public/widgets/r4d.js
@@ -1,4 +1,5 @@
-(function(window, undefined){
+"use strict";
+(function (window, undefined) {
 
     function getScriptData() {
         var scripts = document.getElementsByTagName('script'),
@@ -6,9 +7,10 @@
             id,
             script,
             src,
-            chromeless;
+            chromeless,
+            i;
 
-        for (var i = 0; i < scripts.length; i++) {
+        for (i = 0; i < scripts.length; i += 1) {
             script = scripts[i];
             id = script.getAttribute('data-r4dw-project_id');
             src = script.getAttribute ? script.getAttribute('src') : script.src;
@@ -33,12 +35,13 @@
      * Async load a script with a callback
      */
     function loadScript(url, callback) {
-        var script = document.createElement('script');
+        var script = document.createElement('script'),
+            entry;
         script.type = 'text/javascript';
         script.async = true;
         script.src = url;
 
-        var entry = document.getElementsByTagName('script')[0];
+        entry = document.getElementsByTagName('script')[0];
         entry.parentNode.insertBefore(script, entry);
         if (script.addEventListener) {
             // W3C standard event
@@ -46,7 +49,7 @@
         }
         else {
             // MS Proprietary Event
-            script.attachEvent('onreadystatechange', function() {
+            script.attachEvent('onreadystatechange', function () {
                 if (/complete|loaded/.test(script.readyState)) {
                     callback();
                 }
@@ -64,21 +67,23 @@
     }
 
     function isCssReady(callback) {
-        var testElem = document.createElement('span');
+        var testElem = document.createElement('span'),
+            entry;
         testElem.id = 'development-widget-css-ready';
         testElem.style.color = '#fff';
-        var entry = document.getElementsByTagName('script')[0];
+        entry = document.getElementsByTagName('script')[0];
         entry.parentNode.insertBefore(testElem, entry);
 
         (function poll() {
-            var node = document.getElementById('development-widget-css-ready');
+            var node = document.getElementById('development-widget-css-ready'),
+                value;
             if (window.getComputedStyle) {
                 value = document.defaultView
                     .getComputedStyle(testElem, null)
                     .getPropertyValue('color');
             }
             else if (node.currentStyle) {
-                value = node.currentStyle['color'];
+                value = node.currentStyle.color;
             }
             if (value && value === 'rgb(186, 218, 85)' ||
                 value.toLowerCase() === '#bada55')
@@ -94,29 +99,35 @@
      * Templating!
      * https://github.com/deepsweet/microjungle
      */
-    var microjungle = function(template) {
+    function microjungle(template) {
         var d = document;
 
         // they just doing their job.
         function monkeys(what, who) {
-            var l = what.length;
+            var l = what.length,
+                i,
+                j,
+                el,
+                attrs,
+                k;
 
-            for (var i = 0; i < l; i++) {
-                var j = what[i];
+            for (i = 0; i < l; i += 1) {
+                j = what[i];
 
                 if (j) {
-                    if (typeof j == 'string') {
+                    if (typeof j === 'string') {
                         //who.appendChild(d.createTextNode(j));
                         who.innerHTML += j;
                     } else {
-                        if (typeof j[0] == 'string') {
-                            var el = d.createElement(j.shift()),
-                                attrs = {}.toString.call(j[0]) === '[object Object]' && j.shift(),
-                                k;
+                        if (typeof j[0] === 'string') {
+                            el = d.createElement(j.shift());
+                            attrs = {}.toString.call(j[0]) === '[object Object]' && j.shift();
 
                             if (attrs) {
-                                for(k in attrs) {
-                                    attrs[k] && el.setAttribute(k, attrs[k]);
+                                for (k in attrs) {
+                                    if (attrs[k]) {
+                                        el.setAttribute(k, attrs[k]);
+                                    }
                                 }
                             }
 
@@ -131,91 +142,104 @@
             }
 
             return who;
-        };
+        }
 
         return monkeys(template, d.createDocumentFragment());
-    };
+    }
 
-    function each (obj, iterator, context) {
+    function each(obj, iterator, context) {
         var nativeForEach = Array.prototype.forEach,
-            breaker = {};
-        if (obj == null) return;
-        if (nativeForEach && obj.forEach === nativeForEach) {
-          obj.forEach(iterator, context);
-        } else if (obj.length === +obj.length) {
-          for (var i = 0, l = obj.length; i < l; i++) {
-            if (i in obj && iterator.call(context, obj[i], i, obj) === breaker) return;
-          }
-        } else {
-          for (var key in obj) {
-            if (obj.hasOwnProperty(key)) {
-              if (iterator.call(context, obj[key], key, obj) === breaker) return;
-            }
-          }
+            breaker = {},
+            i,
+            key,
+            l;
+        if (obj === null) {
+            return;
         }
-    };
+        if (nativeForEach && obj.forEach === nativeForEach) {
+            obj.forEach(iterator, context);
+        } else if (obj.length === +obj.length) {
+            for (i = 0, l = obj.length; i < l; i += 1) {
+                if (i in obj && iterator.call(context, obj[i], i, obj) === breaker) {
+                    return;
+                }
+            }
+        } else {
+            for (key in obj) {
+                if (obj.hasOwnProperty(key)) {
+                    if (iterator.call(context, obj[key], key, obj) === breaker) {
+                        return;
+                    }
+                }
+            }
+        }
+    }
 
-    function map (obj, iterator, context) {
+    function map(obj, iterator, context) {
         var results = [],
             nativeMap = Array.prototype.map;
-        if (obj == null) return results;
-        if (nativeMap && obj.map === nativeMap) return obj.map(iterator, context);
-        each(obj, function(value, index, list) {
-          results[results.length] = iterator.call(context, value, index, list);
+        if (obj === null) {
+            return results;
+        }
+        if (nativeMap && obj.map === nativeMap) {
+            return obj.map(iterator, context);
+        }
+        each(obj, function (value, index, list) {
+            results[results.length] = iterator.call(context, value, index, list);
         });
-        if (obj.length === +obj.length) results.length = obj.length;
+        if (obj.length === +obj.length) {
+            results.length = obj.length;
+        }
         return results;
-    };
+    }
 
-    var scriptInfo = getScriptData();
-    var url_parser = document.createElement('a');
-        url_parser.href = scriptInfo.src;
-    var base_url = scriptInfo.src.substr(0, scriptInfo.src.lastIndexOf('/')+1);
-    var api_path = 'project';
-    var project_id = scriptInfo.project_id;
+    var scriptInfo = getScriptData(),
+        url_parser = document.createElement('a'),
+        base_url = scriptInfo.src.substr(0, scriptInfo.src.lastIndexOf('/') + 1),
+        api_path = 'project',
+        project_id = scriptInfo.project_id;
+    url_parser.href = scriptInfo.src;
 
     // Kick off by loading our stylesheet
     // Use the chromeless sheet if requested
     loadStylesheet(base_url + "r4d" + (scriptInfo.chromeless ? "_chromeless" : "") + ".css");
     //Grab reqwest and the dispatch a request for our data
-    loadScript(base_url + "reqwest.js", function(){
+    loadScript(base_url + "reqwest.js", function () {
         reqwest({
             url: [url_parser.protocol +  '/', url_parser.host, api_path, project_id].join('/'),
             type: 'jsonp',
             success: function (resp) {
                 // Wrap out output action in a
                 // check to ensure that our css is loaded
-                isCssReady(function(){
-                    var data = {outputs: resp.results.bindings,
+                isCssReady(function () {
+                    var div = document.createElement('div'),
+                        data = {outputs: resp.results.bindings,
                             proj_title: resp.results.bindings[0].projectTitle.value,
-                            proj_url: resp.results.bindings[0].r4dProject.value};
+                            proj_url: resp.results.bindings[0].r4dProject.value},
+                        template = [
+                            ['div',
+                                ['h3',
+                                    "Resources from Research for Development relating to ",
+                                    data.proj_title],
+                                ['div', {'class': 'list-wrapper'},
+                                    ['ul',
+                                       map(data.outputs, function (item) {
+                                            return ['li',
+                                                       ['a',
+                                                           {
+                                                                'href': item.output.value,
+                                                                'target': '_blank'
+                                                            },
+                                                           item.outputTitle.value
+                                                       ]
+                                                   ];
+                                       })
+                                    ]
+                                ],
+                                ['a', ['span', 'And 5 more...']]
+                            ]
+                        ];
 
-                    var template = [
-                        ['div',
-                            ['h3',
-                                "Resources from Research for Development relating to ",
-                                data.proj_title],
-                            ['div', {'class': 'list-wrapper'},
-                                ['ul',
-                                   map(data.outputs, function(item) {
-                                       return ['li',
-                                                   ['a',
-                                                       {
-                                                           'href': item.output.value,
-                                                           'target': '_blank'
-                                                       },
-                                                       item.outputTitle.value
-                                                   ]
-                                               ];
-                                   }),
-                                ]
-                            ],
-
-                            ['a', ['span', 'And 5 more...']]
-                        ]
-                    ];
-
-                    var div = document.createElement('div');
                     div.className = "development-widget";
                     div.appendChild(microjungle(template));
                     // Render our stuff
@@ -223,14 +247,14 @@
 
                     // Load Emile to do animations
                     // We do this late since the click on the scroll link isn't urgent
-                    loadScript(base_url + "emile.js", function(){
+                    loadScript(base_url + "emile.js", function () {
                         // Deal with clicks on the scroller
                         //It'll be the last element in out div
-                        div.childNodes[0].childNodes[div.childNodes[0].childNodes.length -1].onclick = function (evt, item) {
+                        div.childNodes[0].childNodes[div.childNodes[0].childNodes.length - 1].onclick = function (evt, item) {
                             var ul = div.getElementsByTagName('ul')[0];
                             emile(ul, 'width:-100px', {duration: 300});
                             return false;
-                        }
+                        };
                     });
                 });
             }
